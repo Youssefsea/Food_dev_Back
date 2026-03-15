b91747b6f19782a12ecf1d87a9df49a526a4adf0
يوسف ياسر <yassefsea274@gmail.com>
2026-03-15 13:58:38 +0200

    Refactor CORS handling in start.js

--- start.js
+++ start.js
@@ -2,32 +2,26 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const app = express();
+app.set('trust proxy', 1);
 const router = require('./router');
 const cookieParser = require('cookie-parser');
-const cors = require('cors');
 const { setupChatSocket } = require('./chat/chatSocket');
 
-const allowedOrigins = [
-  "https://food-front-rho.vercel.app"
-];
+const allowedOrigins = ["https://food-front-rho.vercel.app"];
 
-// Define cors options once and reuse
-const corsOptions = {
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-};
+// CORS middleware - must be first
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://food-front-rho.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
 
 const server = http.createServer(app);
-
-
 const io = new Server(server, {
   cors: {
     origin: allowedOrigins,
@@ -37,11 +31,7 @@ const io = new Server(server, {
 });
 
 setupChatSocket(io);
-
 app.use(cookieParser());
-app.use(cors(corsOptions));
-app.options('/{*path}', cors(corsOptions))
-
 app.use(express.json());
 app.use('/', router);
 
